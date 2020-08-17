b7771d76c11fe58ac195485bcd31c7d6af6b20a4
Felipe Mendes <fmagliom@gmail.com>
2020-08-17 20:51:41 -0300

    First deploy

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import 'dotenv/config';
 
 import express from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 
 import routes from './routes';
@@ -12,10 +13,18 @@ import rateLimiter from './routes/middlewares/rateLimiter';
 
 const app = express();
 
+app.use(
+  cors({
+    origin: '*',
+    optionsSuccessStatus: 200,
+  }),
+);
 app.use(express.json());
 app.use(rateLimiter);
 app.use(routes);
 app.use(celebrateErrors());
 app.use(exceptionHandler());
 
-app.listen(3333, () => console.log('Server is running on port 3333!'));
+app.listen(process.env.PORT || 3333, () =>
+  console.log('Server is running on port 3333!'),
+);
